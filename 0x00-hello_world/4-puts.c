fa24bc94fcacf6a6c233d47adf67c7f8627d7d3f
pauloralph <paulebong@ymail.com>
2022-09-08 04:41:08 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <studio.h>
+/**
+ * main . entry point
+ *
+ * return; 0 after printing the function
+ */
+int main(void)
+{puts("/"programming is like building a multilingual puzzle");
+         return (0)
+}	 
