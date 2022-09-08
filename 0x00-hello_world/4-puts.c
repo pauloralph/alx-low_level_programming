4671590dd5ff3fe20df1ae7291ab323fa16ccd47
pauloralph <paulebong@ymail.com>
2022-09-08 12:11:18 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc -S $CFILE
+#include <studio.h>
+/**
+ * main . entry point
+ *
+ * Return: 0 after printing the function 
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle);
+        return (0);
+}	
