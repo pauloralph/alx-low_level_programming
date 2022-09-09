4617ecec27313c97c499b7998b17fd59ebb46279
pauloralph <paulebong@ymail.com>
2022-09-09 04:53:29 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,15 @@
 #include <string.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		char str[] = "\"Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
 
-			puts(str);
+	puts(str);
 
-				return (0);
+	return (0);
 }
