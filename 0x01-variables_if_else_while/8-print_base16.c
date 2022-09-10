5fee3b9bba7e9af2c0a81c7c18dceea9a58753f4
pauloralph <paulebong@ymail.com>
2022-09-10 05:31:34 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * program that prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int numb;
+	char alphab;
+
+	for (numb = 48; numb <= 57; numb++)
+	putchar(numb);
+	for (alphab = 97; alphab <= 102; alphab++)
+	putchar(alphab);
+	putchar('\n');
+	return (0);
+}
