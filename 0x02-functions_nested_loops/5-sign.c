33f9987da8a1205d915198f93a3158da0c1e33e8
pauloralph <paulebong@ymail.com>
2022-09-13 18:16:03 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - Begin here
+ *
+ * return 1 if lower case and zero if not
+ *
+ * @n: interger value
+ *
+ * Return: always 0 (success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+			_putchar('0');
+			return (0);
+	}
+	else
+	{
+			_putchar('-');
+			return (-1);
+	}
+}
+
