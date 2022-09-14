19897ef8bf954b69a78473188db9ac226597eb5f
pauloralph <paulebong@ymail.com>
2022-09-14 09:23:20 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,18 +5,17 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a >= b && a >= c)
+if (a >= b && b >= c)
 {
 largest = a;
 }
-else if (b >= a && b >= c)
+else if ( >= a && b >= c)
 {
 largest = b;
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,12 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+void print_times_table(int n);
 
 #endif
