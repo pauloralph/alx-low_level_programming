a634206b60b3103fd7fd985a03bbd380a4ea5711
pauloralph <paulebong@ymail.com>
2022-09-13 19:14:50 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - a function that add two intergers and returns the result
+ *
+ * @x: number one
+ *
+ * @y: number two
+ *
+ * Return: add number one and two
+ */
+int add(int x, int y)
+
+{
+	return (x + y);
+
+}
