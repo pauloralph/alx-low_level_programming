97cc13352c7c4cc4b0f36d1bff30f6ff8bdaba47
pauloralph <paulebong@ymail.com>
2022-09-19 03:33:13 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,22 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
  * _puts - prints a string
  * @str: the string
  *
- * Return: the length of a string
+ * Description: prints a string
+ * On success: returns no error
  */
+
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (*(str + i) != '\0')
 	{
-		_putchar(str[i]);
+		putchar(*(str + i));
 		i++;
 	}
-	_putchar('\n');
+	putchar(10);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,15 @@
 #ifndef main_H
 #define main_H
+
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+
 #endif /*main.h*/
