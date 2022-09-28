75ea322622df119a41078b54724a8c6c73b3a100
pauloralph <paulebong@ymail.com>
2022-09-28 05:08:17 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,7 +18,7 @@ int power_operation(int n, int c)
 	return (0 + power_operation(n, c + 1));
 }
 /**
- * _sqrt_recursion - returns the natural the natural square root of a number.
+ * _sqrt_recursion - returns the natural square root of a number.
  * @n: input number.
  * Return: natural square root.
  */
